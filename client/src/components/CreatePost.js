07f61ba5a913158428b2c063674a37170fc6298b
Erik Wehrmann <erik.wehrmann@gmail.com>
2023-02-05 09:13:00 -0500

    createpost

---
+++ client/src/components/CreatePost.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function CreatePost() {
+  return (
+    <div>
+      <form onSubmit={(event) => event.preventDefault()}>
+        <input name="title" type="text" placeholder="Title" />
+        <input name="address" type="text" placeholder="Address" />
+        <input name="picture_url" type="test" placeholder="Picture" />
+        <input name="entry" type="text" placeholder="Journal Entry" />
+      </form>
+      <button onClick={console.log("submit")}>Submit</button>
+    </div>
+  );
+}
