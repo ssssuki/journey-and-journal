83d67bb218fafe146494a22ffd4b2293c2086c72
Karen Mui <KarenMui@Karens-MacBook-Pro.local>
2023-02-05 05:04:30 -0500

    working on retrieving server data in front end

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const db = require('../db/connection');
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("index", { title: "Express" });
+  db.query(`
+  SELECT (posts.*) FROM posts
+  ORDER BY rating DESC;
+  `)
+  .then(result => {
+    res.json(result.rows);
+  })
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
+  });
 });
 
 module.exports = router;
