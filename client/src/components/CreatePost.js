b4019d250ff6f35ffc04ab98f5936f3d9801addc
Erik Wehrmann <erik.wehrmann@gmail.com>
2023-02-05 12:21:36 -0500

    setting up the post to api

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CreatePost() {
+  const [state, setState] = useState({
+    title: "",
+    address: "",
+    picture_url: "",
+    entry: "",
+  });
+  function submitPost() {
+    console.log(state);
+  }
   return (
     <div>
       <form onSubmit={(event) => event.preventDefault()}>
-        <input name="title" type="text" placeholder="Title" />
-        <input name="address" type="text" placeholder="Address" />
-        <input name="picture_url" type="test" placeholder="Picture" />
-        <input name="entry" type="text" placeholder="Journal Entry" />
-        <></>
+        <input
+          name="title"
+          type="text"
+          placeholder="Title"
+          value={state.title}
+          onChange={(event) =>
+            setState({ ...state, title: event.target.value })
+          }
+        />
+        <input
+          name="address"
+          type="text"
+          placeholder="Address"
+          value={state.address}
+          onChange={(event) =>
+            setState({ ...state, address: event.target.value })
+          }
+        />
+        <input
+          name="picture_url"
+          type="text"
+          placeholder="Picture"
+          value={state.picture_url}
+          onChange={(event) =>
+            setState({ ...state, picture_url: event.target.value })
+          }
+        />
+        <input
+          name="entry"
+          type="text"
+          placeholder="Journal Entry"
+          value={state.entry}
+          onChange={(event) =>
+            setState({ ...state, entry: event.target.value })
+          }
+        />
       </form>
-      <button onClick={console.log("submit")}>Submit</button>
+      <button onClick={() => submitPost()}>Post</button>
     </div>
   );
 }
