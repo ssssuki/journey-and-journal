ced8a7dcaf1b9e95ad7fd38f5cf13f5c5700ceaf
Karen Mui <KarenMui@Karens-MacBook-Pro.local>
2023-02-06 16:00:55 -0500

    set state and able to display the data

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 export default function Home () {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/')
+    .then(res => {
+      console.log(res.data);
+      setPosts(res.data);
+    });
+  }, []);
+
+
   return(
-    <div>I am home component</div>
+    <div>I am home component
+      {posts.map(post => <li>{ post.id } { post.photo_link } {post.locality} {post.title}</li>)}
+    </div>
   )
 }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,17 +1,7 @@
-import { useEffect } from 'react';
-import axios from 'axios';
-
-// Component
 import Home from "../components/Home"
 
 export default function HomePage() {
 
-  useEffect(() => {
-    axios.get('http://localhost:8080/').then(res => {
-      console.log('hi')
-      console.log(res.data);
-    });
-  }, []);
 
   return <h1>"I am home page!"
     <Home />
