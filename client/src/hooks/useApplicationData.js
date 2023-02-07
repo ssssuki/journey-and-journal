7e5db097ebd3f2a48d71f4e160307b08521ce76c
Ssophia <susu94827@gmail.com>
2023-02-06 19:59:46 -0500

    added api for comments

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -6,19 +6,23 @@ export default function useApplicationData(props) {
 
   const [state, setState] = useState({
     posts: {},
-    isLoading: true
+    comments:{},
+    isLoading:true
   });
 
-  useEffect(() =>{
-    axios.get("http://localhost:8080/api/posts").then((res) => {
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8080/api/posts"),
+      axios.get("http://localhost:8080/api/comments"),
+    ]).then((all) => {
       setState((prev) => ({
         ...prev,
-        posts: res.data,
+        posts: all[0].data,
+        comments: all[1].data,
         isLoading: false
-      }))
-    })
-  }, [setState])
-
+      }));
+    });
+  }, []);
   
 
   return {
