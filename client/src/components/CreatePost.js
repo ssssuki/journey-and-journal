0e10ce449b1ebbfa2a0f655fba017df7d950cfbc
Erik Wehrmann <erik.wehrmann@gmail.com>
2023-02-07 13:14:24 -0500

    nav bar, logo redirects

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,4 +1,5 @@
 import React, { useState, useCallback } from "react";
+import "../styles/createPost.scss";
 import {
   GoogleMap,
   useJsApiLoader,
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,54 +1,62 @@
 import React, { useState } from "react";
 import useUser from "../hooks/useUser";
-// import { useCookies } from "react-cookie";
-// import setUser from "../hooks/setUser";
+import "../styles/navbar.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar(props) {
   const [search, setSearch] = useState("");
-  // const [cookies, setCookie, removeCookie] = useCookies(["session"]);
 
   const { login, logout, cookies } = useUser();
+
+  const navigate = useNavigate();
 
   const handleSearch = (search) => {
     console.log(search);
   };
-
-  // const login = (id) => {
-  //   const user = setUser(id).then(() => {
-  //     setCookie("session", user);
-  //     console.log(cookies.session);
-  //   });
-  // };
-
-  // const logout = () => {
-  //   removeCookie("session");
-  //   console.log(cookies.session);
-  // };
-
   return (
-    <div>
-      <h1>Logo</h1>
-      <form onSubmit={(event) => event.preventDefault()}>
-        <input
-          name="search-query"
-          type="text"
-          placeholder="Search by Location!"
-          value={search}
-          onChange={(event) => setSearch(event.target.value)}
-        />
-      </form>
-      <button onClick={() => handleSearch(search)}>Go!</button>
-      {!cookies.session ? (
-        <>
-          <button onClick={() => login(2)}>Login As Bob</button>
-          <button onClick={() => login(3)}>Login As Jessie</button>
-        </>
-      ) : (
-        <>
-          <span>Logged in as {cookies.session.username}</span>
-          <button onClick={() => logout()}>Logout</button>
-        </>
-      )}
+    <div className="nav-bar">
+      <div className="nav-bar-item">
+        <h1 onClick={() => navigate("/")}>Logo</h1>
+      </div>
+      <div className="nav-bar-item">
+        <form
+          classname="search-bar"
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <input
+            name="search-query"
+            type="text"
+            placeholder="Search by Location!"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+        </form>
+        <button
+          className="btn btn-primary"
+          onClick={() => handleSearch(search)}
+        >
+          Go!
+        </button>
+      </div>
+      <div className="nav-bar-item">
+        {!cookies.session ? (
+          <>
+            <button className="btn btn-secondary" onClick={() => login(2)}>
+              Login As Bob
+            </button>
+            <button className="btn btn-secondary" onClick={() => login(3)}>
+              Login As Jessie
+            </button>
+          </>
+        ) : (
+          <>
+            <span>Logged in as {cookies.session.username}</span>
+            <button className="btn btn-secondary" onClick={() => logout()}>
+              Logout
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
