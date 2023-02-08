9d2c7bfd64a50aa7f545409557e355cabb571701
Erik Wehrmann <erik.wehrmann@gmail.com>
2023-02-08 12:57:01 -0500

    fixed merge conflict

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,7 +7,6 @@ module.exports = (db) => {
     });
   });
   router.get("/posts/:id", (request, response) => {
-<<<<<<< HEAD
     db.query(`SELECT * FROM posts WHERE id=${request.params.id};`).then(
       ({ rows: posts }) => {
         response.json(posts);
@@ -33,11 +32,6 @@ module.exports = (db) => {
     )
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
-=======
-    db.query(`SELECT * FROM posts WHERE id=${request.params.id}`).then(({ rows: posts }) => {
-      response.json(posts);
-    });
->>>>>>> 6ad548548fef46e7056aebbc5c0ac1a99d40caec
   });
   return router;
 };
