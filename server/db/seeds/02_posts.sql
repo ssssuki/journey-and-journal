c150c3559721e8e4ead0fa48938a27a7f3cfeacd
Ssophia <susu94827@gmail.com>
2023-02-13 16:32:45 -0500

    updateds more posts to the database

--- server/db/seeds/02_posts.sql
+++ server/db/seeds/02_posts.sql
@@ -2,9 +2,9 @@ INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitud
 VALUES (
   1, 
   'CN Tower', 
-  'Today I went to the CN Tower', 
+  'The Tower is located right next to Rogers Center and the Ripley’s Aquarium , and just 2 minutes from the lakeshore. This a must see if your visiting Toronto.', 
   4, 
-  'https://www.connectingtraveller.com/images/localtip/1665245016carson-van-tol-y-RCl_SWWuo-unsplash.jpg', 
+  'https://seeyousoon.ca/wp-content/uploads/2020/07/Toronto-Skyline-1200x788.jpg', 
   43.641566, 
   -79.387057, 
   'Toronto'
@@ -14,9 +14,9 @@ INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitud
 VALUES (
   2, 
   'Mont Tremblant', 
-  'Fun place to ski', 
+  'Fun place to ski!', 
   5, 
-  'https://a.travel-assets.com/findyours-php/viewfinder/images/res70/494000/494746-mont-tremblant.jpg?impolicy=fcrop&w=1040&h=580&q=mediumHigh', 
+  'https://www.skimag.com/wp-content/uploads/2022/10/MontTremblant-1450-200109-HEBDO-SOUL-AVTSMT-KLMOTIONMEDIA-1-7.jpg', 
   46.118462, 
   -74.596185, 
   'Mont-Tremblant'
@@ -26,7 +26,7 @@ INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitud
 VALUES (
   3, 
   'Empire State Building', 
-  'Big scary tower', 
+  'Nice tour of the building along with interesting picture spots within.', 
   1, 
   'https://www.ourescapeclause.com/wp-content/uploads/2019/02/NewYorkJan2019-47.jpg', 
   40.748441,
@@ -38,7 +38,7 @@ INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitud
 VALUES (
   2, 
   'Casa Loma', 
-  'Who built a castle in toronto', 
+  'Grounds are beautiful to walk through.', 
   4, 
   'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/3a/0a/7b/casa-loma-gardens.jpg?w=2000&h=-1&s=1', 
   43.678037,
@@ -62,10 +62,84 @@ INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitud
 VALUES (
   1, 
   'Parliament', 
-  'Super underwhelming', 
-  2, 
+  'Super underwhelming!', 
+  4, 
   'https://cdn.britannica.com/29/179429-050-EDBCAE49/Parliament-Buildings-Ottawa.jpg', 
   45.423594,
   -75.700929,
   'Ottawa'
+  );
+
+
+INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitude, address) 
+VALUES (
+  1, 
+  'The Grand Palace', 
+  'It’s one must-see sight that no visit to the city would be complete without.', 
+  4, 
+  'https://a.cdn-hotels.com/gdcs/production88/d1534/da0c22f3-443f-4b20-b38c-ee6d2ba97962.jpg?impolicy=fcrop&w=1600&h=1066&q=medium', 
+  13.750300,
+  100.491180,
+  'Bangkok'
+  );
+
+
+INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitude, address) 
+VALUES (
+  2, 
+  'Louvre Museum', 
+  'Probably the most famous museum in the world.', 
+  5, 
+  'https://media.cntraveler.com/photos/57d961ce3e6b32bf25f5ad0f/16:9/w_1920,c_limit/most-beautiful-paris-louvre-GettyImages-536267205.jpg', 
+  48.860294,
+  2.338629,
+  'Paris'
+  );
+
+INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitude, address) 
+VALUES (
+  2, 
+  'London Eye', 
+  'Impressed how well the queue was managed and how speedily we got on.', 
+  3, 
+  'https://misstourist.com/wp-content/uploads/2022/09/0-Best-tour-for-London-Eye-for-couples-660x440@2x.jpg', 
+  51.503399,
+  -0.119519,
+  'London'
+  );
+
+INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitude, address) 
+VALUES (
+  3, 
+  'Fushimi Inari-taisha', 
+  'Beautiful place, lots of street food on the way.', 
+  4, 
+  'https://s3-ap-northeast-1.amazonaws.com/thegate/2021/02/10/12/20/27/Fushimiinari-Shrine.jpg', 
+  34.9670 ,
+  135.7703,
+  'Kyoto'
+  );
+
+  INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitude, address) 
+VALUES (
+  3, 
+  'Shinjuku', 
+  'Very busy place!', 
+  3, 
+  'https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/4/2017/03/Shinjuku-City-512910636-1024x693.jpg', 
+  35.6901 ,
+  139.6895,
+  'Tokyo'
+  );
+
+  INSERT INTO posts (user_id, title, entry, rating, photo_link, latitude, longitude, address) 
+VALUES (
+  1, 
+  'Waialua', 
+  'This beach was extremely beautiful.The water is clearly blue and the shade is everywhere .', 
+  5, 
+  'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1746&q=80', 
+  21.1003, 
+   -156.7614, 
+  'Hawaii'
   );
--- server/db/seeds/04_likes.sql
+++ server/db/seeds/04_likes.sql
@@ -3,5 +3,3 @@ INSERT INTO likes (user_id, post_id) VALUES (3, 3);
 INSERT INTO likes (user_id, post_id) VALUES (2, 1);
 INSERT INTO likes (user_id, post_id) VALUES (2, 2);
 INSERT INTO likes (user_id, post_id) VALUES (2, 4);
-INSERT INTO likes (user_id, post_id) VALUES (5, 1);
-INSERT INTO likes (user_id, post_id) VALUES (4, 1);
