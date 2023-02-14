bd34dbfdbd54daf7467d04b55a5e0477b88d28c4
Ssophia <susu94827@gmail.com>
2023-02-13 22:51:38 -0500

    updated seeds

--- server/db/seeds/04_likes.sql
+++ server/db/seeds/04_likes.sql
@@ -1,5 +1,6 @@
 INSERT INTO likes (user_id, post_id) VALUES (3, 2);
-INSERT INTO likes (user_id, post_id) VALUES (3, 3);
+INSERT INTO likes (user_id, post_id) VALUES (3, 6);
 INSERT INTO likes (user_id, post_id) VALUES (2, 1);
-INSERT INTO likes (user_id, post_id) VALUES (2, 2);
-INSERT INTO likes (user_id, post_id) VALUES (2, 4);
+INSERT INTO likes (user_id, post_id) VALUES (2, 5);
+INSERT INTO likes (user_id, post_id) VALUES (2, 7);
+INSERT INTO likes (user_id, post_id) VALUES (1, 8);
